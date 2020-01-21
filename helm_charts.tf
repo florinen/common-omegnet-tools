d9cb08e91867a0351eef1cfb6712d5d9fe2e4b71
florinen <nenfl24@gmail.com>
2020-01-20 21:19:43 -0600

    added more charts

--- helm_charts.tf
+++ helm_charts.tf
@@ -3,18 +3,18 @@ data "helm_repository" "stable" {
   depends_on = ["null_resource.helminit"]
   name       = "stable"
   #url  = "${var.public_url}"
-  url        = "${var.private_url}"
+  url = "${var.private_url}"
 }
 
 resource "helm_release" "metallb" {
-  depends_on = ["data.helm_repository.stable"]
-  name       = "metallb"
-  namespace  = "metallb-system"
+  #depends_on = ["data.helm_repository.stable"]
+  name         = "${var.metallb_name}"
+  namespace    = "${var.metallb_namespace}"
   force_update = true
-  repository = data.helm_repository.stable.metadata[0].name
-  chart      = "${path.module}/${var.metallb_chart}"
-  wait       = false
-  
+  repository   = data.helm_repository.stable.name
+  chart        = "stable/${var.metallb_chart}"
+  wait         = false
+
   # values = [
   #   "${file("configInline.yaml")}"
   # ]
@@ -23,12 +23,34 @@ resource "helm_release" "metallb" {
   #     value = 
   #   }
 }
-# resource "helm_release" "consul" {
-#     name      = "${var.consul_name}"
-#     namespace = "${var.namespace}"
-#     chart     = "${path.module}/consul"
-# }
+resource "helm_release" "ingress_controller" {
+  name         = "${var.ingress_name}"
+  namespace    = "${var.ingress_namespace}"
+  force_update = true
+  repository   = data.helm_repository.stable.name
+  chart        = "stable/${var.ingress_chart}"
+  wait         = false
+  #version   = "${var.ingress_version}"
+}
 # set {
 #     name = "consul-port"
 #     value = "${var.consul_service_port}"
-#   }
+# }  
+resource "helm_release" "nfs_client_provisioner" {
+  name         = "${var.nfs_clinet_name}"
+  namespace    = "${var.nfs_client_namespace}"
+  force_update = true
+  repository   = data.helm_repository.stable.name
+  chart        = "stable/${var.nfs_client_chart}"
+  wait         = false
+  #version     = "${}"
+}
+resource "helm_release" "metrics_server" {
+  name         = "${var.metrics_server_name}"
+  namespace    = "${var.metrics_server_namespace}"
+  force_update = true
+  repository   = data.helm_repository.stable.name
+  chart        = "stable/${var.metrics_server_chart}"
+  wait         = false
+  #version     = "${}"
+}
