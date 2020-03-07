4c5ad1655f96c74928a82c7fe519111b8fa3e5cc
Florin <nenfl24@gmail.com>
2020-03-07 14:21:18 -0600

    remoded one line

--- remote_state.tf
+++ remote_state.tf
@@ -5,7 +5,6 @@ data "terraform_remote_state" "base_config" {
 
   config = {
     bucket  = "kube.omegnet.com"
-    key     = "vsphere/prod/base-tools/base-tools.tfstate"
     key    = "${var.provider_name}/${var.deployment_environment}/${var.deployment}/${var.state_file}"
     region  = "${var.region}"
   }
